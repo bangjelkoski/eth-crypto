28b43cee411175ed9a20e667d041b5d3f9eec3d2
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-11-06 16:26:45 +0100

    chore: migrate eccrypto

--- dist/lib/decrypt-with-private-key.js
+++ dist/lib/decrypt-with-private-key.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.decryptWithPrivateKey = decryptWithPrivateKey;
-var _eccrypto = require("eccrypto");
+var _eccrypto = require("@toruslabs/eccrypto");
 var _cipher = require("./cipher");
 var _util = require("./util");
 function decryptWithPrivateKey(privateKey, encrypted) {
@@ -13,12 +13,12 @@ function decryptWithPrivateKey(privateKey, encrypted) {
   // remove trailing '0x' from privateKey
   var twoStripped = (0, _util.removeLeading0x)(privateKey);
   var encryptedBuffer = {
-    iv: Buffer.from(encrypted.iv, 'hex'),
-    ephemPublicKey: Buffer.from(encrypted.ephemPublicKey, 'hex'),
-    ciphertext: Buffer.from(encrypted.ciphertext, 'hex'),
-    mac: Buffer.from(encrypted.mac, 'hex')
+    iv: Buffer.from(encrypted.iv, "hex"),
+    ephemPublicKey: Buffer.from(encrypted.ephemPublicKey, "hex"),
+    ciphertext: Buffer.from(encrypted.ciphertext, "hex"),
+    mac: Buffer.from(encrypted.mac, "hex")
   };
-  return (0, _eccrypto.decrypt)(Buffer.from(twoStripped, 'hex'), encryptedBuffer).then(function (decryptedBuffer) {
+  return (0, _eccrypto.decrypt)(Buffer.from(twoStripped, "hex"), encryptedBuffer).then(function (decryptedBuffer) {
     return decryptedBuffer.toString();
   });
 }
